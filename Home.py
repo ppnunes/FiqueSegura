c4fae69debf55cdbb4a125c94717aee202557698
Priscila Nunes <priscilapereiranunes@gmail.com>
2025-03-19 12:32:49 -0300

    Draft from mockup
    Signed-off-by: Priscila Nunes <priscilapereiranunes@gmail.com>

---
+++ Home.py
@@ -0,0 +1,50 @@
+import streamlit as st
+
+def enable_map():
+    st.session_state.mostrar_mapa = True
+    st.success('Mapa carregado com sucesso!')
+    st.table(st.session_state)
+
+def disable_map():
+    st.session_state.mostrar_mapa = False
+
+st.set_page_config(
+    page_title="Fique Segura",
+    page_icon=":balloon:",
+    # layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'About': """# O que é o Fique Segura?
+O Fique Segura é um aplicativo que tem como objetivo auxiliar mulheres vítimas de violência."""
+    }
+)
+
+# Inicialize a variável de sessão como False
+if'mostrar_mapa' not in st.session_state:
+    st.session_state.mostrar_mapa = False
+    st.session_state.estado = None
+    st.session_state.cidade = None
+    st.session_state.municipio = None
+
+st.title('Fique Segura!')
+
+st.markdown("""
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+""")
+
+st.info('Lorem ipsum dolor sit amet', icon="ℹ️")
+
+mapa_container = st.container()
+if st.session_state.mostrar_mapa:
+    with mapa_container:
+        st.map()
+
+st.selectbox('Estado:', ['Opção 1', 'Opção 2', 'Opção 3'], key='estado', on_change=disable_map, placeholder='Selecione uma opção')
+st.selectbox('Cidade:', ['Opção 1', 'Opção 2', 'Opção 3'], key='cidade', on_change=disable_map, placeholder='Selecione uma opção')
+st.selectbox('Município:', ['Opção 1', 'Opção 2', 'Opção 3'], key='municipio', on_change=disable_map, placeholder='Selecione uma opção')
+
+if st.session_state.mostrar_mapa:
+    st.button('Nova busca', on_click=disable_map)
+else:
+    st.button('Buscar', on_click=enable_map,disabled=not st.session_state.estado or not st.session_state.cidade or not st.session_state.municipio)
+
---
+++ pages/Sobre.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('Fique Segura!')
+
+st.markdown("""
+    ## O que é o Fique Segura?
+    O Fique Segura é um aplicativo que tem como objetivo auxiliar mulheres vítimas de violência.
+""")
